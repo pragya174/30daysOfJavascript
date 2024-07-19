34cc7d6b0dcf914e85ec9fb22d8020f27540d294
Your Name <pragya4429@gmail.com>
2024-07-19 11:14:49 +0530

    added day1.js file

---
+++ day1.js
@@ -0,0 +1,44 @@
+// Activity 1: Variable Declaration
+// -> Task 1:
+var num1 = 6;
+console.log(num1);
+
+// -> Task 2:
+let name1 = "Pragya";
+console.log(name1);
+
+// Activity 2: Constant Declaration
+// -> Task 3:
+const x = true;
+console.log(x);
+
+// Activity 3: Data Types
+// -> Task 4:
+let num = 5; //number
+let str = "hello"; //string
+let b = false; //boolean
+let obj = {  //object
+    name: 'Shiya',
+    email: 'shiya@gamil.com'
+}
+let fruits = ['mango','apple','lichi','grapes'];  //array
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(b));
+console.log(typeof(obj));
+console.log(typeof(fruits));
+
+// Activity 4: Reassinging Variables
+// -> Task 5:
+let num2 = 7;
+console.log(num2);
+num2 = 12;
+console.log(num2);
+
+//Activity 5: Understanding const
+// -> Task 6:
+const str2 = "heyy";
+console.log(str2);
+str2 = "world";
+console.log(str2);
+
